e0127f9a6aa59557b586baf9150bd312ec9c10a1
YJChen2021 <93013231+YJChen2021@users.noreply.github.com>
2024-04-18 15:39:01 +0800

    Update main.c

--- I2C_Communication/main.c
+++ I2C_Communication/main.c
@@ -1,1 +1,216 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "nbsdk.h"
+#include "bsp.h"
+#include "nbsdk_api.h"
+#include "User_Config.h"
+#include "include/sensor_aeg.h"
+#include "include/Basic.h"
+#include "Token.h"
 
+/**************************************************************************
+ *	System Constants
+ **************************************************************************/
+#define HOST_SECTION __attribute__((section(".ver_host")))
+#define TASK_DELAY_TIME 2
+#define TRUE 1
+#define FALSE 0
+
+#define C1 (5.16e-05f)
+#define C2 (0.008801f)
+#define C3 (0.4602f)
+#define C4 (9.72f)
+
+/**************************************************************************
+ *	External Function
+ **************************************************************************/
+extern void debug_enable_box(int id, int en, int sx, int sy, int ex, int ey);
+extern void debug_set_color(int color); // change word color black 0, white 255
+extern void debug_enable_label(int id, int en, int value, int sx, int sy);
+extern unsigned long intToBcd(int input);
+/**************************************************************************
+ *	Debug Configuration
+ **************************************************************************/
+
+//#define image_viewer
+
+/**************************************************************************
+ *	Image Processing constant
+ **************************************************************************/
+#define ROI_X 100
+#define ROI_Y 0
+#define ROI_W 120
+#define ROI_H 320
+#define Binary_threshold 128
+/**************************************************************************
+ *	Global Data
+ **************************************************************************/
+static const char HOST_SECTION host_ver[] = "HOST-"__DATE__"-"__TIME__;
+
+static TaskHandle_t xHandle_Win = NULL;
+static TaskHandle_t xHandle_User = NULL;
+static TaskHandle_t xHandle_Feature = NULL;
+static TaskHandle_t xHandle_Result = NULL;
+
+unsigned long int win_flow = 0;
+unsigned long int detected_flow = 0;
+unsigned long int frame_id = 0;
+
+u32 Sys_Tick = 0;
+u8 task_init = FALSE;
+u8 process_done = 1;
+
+/**************************************************************************
+ *	Function
+ **************************************************************************/
+
+static void custom_gpio_2(void){
+	sys_set_padshare(IOA3, PAD_FUNC5, PAD_PULL_NO_PULL, PAD_STRENGTH_DIS);
+	gpio_set_dir(IOA3, GPIO_INPUT_DIRECTION);
+
+	// Flash
+	sys_set_padshare(IOB1, PAD_FUNC0, PAD_PULL_NO_PULL, PAD_STRENGTH_DIS);
+	sys_set_padshare(IOB2, PAD_FUNC0, PAD_PULL_NO_PULL, PAD_STRENGTH_DIS);
+	sys_set_padshare(IOB3, PAD_FUNC0, PAD_PULL_NO_PULL, PAD_STRENGTH_DIS);
+	sys_set_padshare(IOB4, PAD_FUNC0, PAD_PULL_NO_PULL, PAD_STRENGTH_DIS);
+	sys_set_padshare(IOB5, PAD_FUNC0, PAD_PULL_NO_PULL, PAD_STRENGTH_DIS);
+	sys_set_padshare(IOB6, PAD_FUNC0, PAD_PULL_NO_PULL, PAD_STRENGTH_DIS);
+
+	sys_set_padshare(IOA6, PAD_FUNC0, PAD_PULL_NO_PULL, PAD_STRENGTH_DIS);
+	gpio_set_dir(IOA6, GPIO_OUTPUT_DIRECTION);
+	sys_set_padshare(IOA5, PAD_FUNC0, PAD_PULL_NO_PULL, PAD_STRENGTH_DIS);
+	gpio_set_dir(IOA5, GPIO_INPUT_DIRECTION);
+	uart_init(115200,UART_LCR_8N1,2);
+	
+	sys_set_padshare(IOA0, PAD_FUNC5, PAD_PULL_NO_PULL, PAD_STRENGTH_DIS);
+	gpio_set_dir(IOA0, GPIO_INPUT_DIRECTION);
+
+	sys_set_padshare(IOA1, PAD_FUNC5, PAD_PULL_NO_PULL, PAD_STRENGTH_DIS);
+	gpio_set_dir(IOA1, GPIO_INPUT_DIRECTION);
+
+	sys_set_padshare(IOA2, PAD_FUNC5, PAD_PULL_NO_PULL, PAD_STRENGTH_DIS);
+	gpio_set_dir(IOA2, GPIO_INPUT_DIRECTION);
+
+	sys_set_padshare(IOB7, PAD_FUNC5, PAD_PULL_NO_PULL, PAD_STRENGTH_DIS);
+	gpio_set_dir(IOB7, GPIO_INPUT_DIRECTION);
+
+	sys_set_padshare(IOC0, PAD_FUNC5, PAD_PULL_NO_PULL, PAD_STRENGTH_DIS);
+	gpio_set_dir(IOC0, GPIO_INPUT_DIRECTION);
+
+	sys_set_padshare(IOC1, PAD_FUNC5, PAD_PULL_NO_PULL, PAD_STRENGTH_DIS);
+	gpio_set_dir(IOC1, GPIO_INPUT_DIRECTION);
+
+	sys_set_padshare(IOC2, PAD_FUNC5, PAD_PULL_NO_PULL, PAD_STRENGTH_DIS);
+	gpio_set_dir(IOC2, GPIO_INPUT_DIRECTION);
+
+	sys_set_padshare(IOC3, PAD_FUNC5, PAD_PULL_NO_PULL, PAD_STRENGTH_DIS);
+	gpio_set_dir(IOC3, GPIO_INPUT_DIRECTION);
+
+	sys_set_padshare(IOC4, PAD_FUNC5, PAD_PULL_NO_PULL, PAD_STRENGTH_DIS);
+	gpio_set_dir(IOC4, GPIO_INPUT_DIRECTION);
+
+	sys_set_padshare(IOC5, PAD_FUNC5, PAD_PULL_NO_PULL, PAD_STRENGTH_DIS);
+	gpio_set_dir(IOC5, GPIO_INPUT_DIRECTION);
+//#endif
+}
+
+static int sensor_setup(void)
+{
+	uint8_t *pinfo = nb_malloc(64);
+	ASSERT(pinfo);
+	verNbsdkGet(pinfo);
+	DEBUG(DEBUG_INFO, "NBSDK_VERSION:%s", pinfo);
+	DEBUG(DEBUG_INFO, "Build @%s", host_ver);
+	osMemInfoDump(0);
+	unsigned int aeg_ver_len;
+	aeg_ver_len = sensor_aeg_get_version(pinfo, 64);
+
+	if (aeg_ver_len > 0)
+	{
+		DEBUG(DEBUG_INFO, "AEG Version: %s", pinfo);
+	}
+	else
+	{
+		DEBUG(DEBUG_ERROR, "AEG Version is Error!!!");
+	}
+	nb_free(pinfo);
+	//token.h
+	sensor_aeg_init(AEG_MODE, AEG_AC_POWER_FREQ, AEG_GAMMA_MODE);
+	sensor_aeg_video_debug_label_enable(TRUE);
+
+	return FALSE;
+}
+
+static void User_Task(void *parameters){
+	#define I2C_slave_address 0x51
+	sys_set_padshare(IOA3, PAD_FUNC1, PAD_PULL_UP, PAD_STRENGTH_DIS);
+	sys_set_padshare(IOA4, PAD_FUNC1, PAD_PULL_UP, PAD_STRENGTH_DIS);
+	i2c_init();
+	i2c_set_speed_mode(I2C_STANDARD_SPEED_MODE);
+	uint8_t flag;
+	uint32_t slave_register_add = 9;
+	uint32_t val = 13;
+	uint32_t rval;
+	uint8_t send_buf[8];
+	uint8_t read_buf[8];
+	//i2c_write_reg8(I2C_slave_address, slave_register_add, 0x17);
+	for (;;){
+		if (task_init){
+			printf("\n");
+			printf("test\n");
+			//flag = i2c_write_reg8(I2C_slave_address, slave_register_add, val&0xff);
+			//if(flag){
+			//	printf("write failed\n");
+			//}
+			//else{
+				flag = i2c_read_reg8(I2C_slave_address, slave_register_add, &rval);
+				printf("flag = %d", flag);
+				if(!flag){
+					printf("write = %x, read = %x \n", val, rval);
+				}
+				else{
+					printf("read failed\n");
+				}
+			//}
+		}
+		vTaskDelay(5000);
+	}
+	vTaskDelete(NULL);
+}
+
+static void userTaskInit(void)
+{
+	BaseType_t xTask_User;
+	xTask_User = xTaskCreate(
+		User_Task,
+		"User_Task",
+		configMINIMAL_STACK_SIZE * 5,
+		NULL,
+		tskIDLE_PRIORITY + 5,
+		&xHandle_User);
+	if (xTask_User != pdPASS)
+	{
+		DEBUG(DEBUG_ERROR, "User_Task is NULL!");
+		exit(FALSE);
+	}
+}
+
+int main(void){
+	SetDebugLevel(DEBUG_LEVEL, DETAIL_INFO_SUPPORT);
+	struct Init_Config *BasicInitConfig = get_init_config();
+	BasicInitConfig->BaudRate = UART_DEFAULT_BAUD_RATE_INDEX;
+	BasicInitConfig->SPISpeed = FLASH_SPI_SPEED;
+	BasicInitConfig->CISWorkFreq = SYS_WORK_FREQ;
+	BasicInitConfig->SystemInfoDump = SYS_INFO_DUMP_SW;
+	BasicInitConfig->IOPortingMode = IO_PORTING_MODE;
+	BasicInitConfig->DebugMode = DEBUG_MODE;
+	BasicInitConfig->BLCEnable = BLC_SW;
+	InitializeBase(BasicInitConfig);
+#ifndef image_viewer
+	custom_gpio_2();
+#endif
+	userTaskInit();
+	task_init = TRUE;
+	return 0;
+}
