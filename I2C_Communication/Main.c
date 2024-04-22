ea317f529c3e2b36c2ebcb7b3149c5bbae7db203
YJChen2021 <93013231+YJChen2021@users.noreply.github.com>
2024-04-22 14:35:49 +0800

    Update Main.c

--- I2C_Communication/Main.c
+++ I2C_Communication/Main.c
@@ -136,7 +136,7 @@ static int sensor_setup(void)
 }
 
 static void User_Task(void *parameters){
-	#define I2C_slave_address 0x51
+	#define I2C_slave_address 0x51 //adjustable
 	sys_set_padshare(IOA3, PAD_FUNC1, PAD_PULL_UP, PAD_STRENGTH_DIS);
 	sys_set_padshare(IOA4, PAD_FUNC1, PAD_PULL_UP, PAD_STRENGTH_DIS);
 	i2c_init();
